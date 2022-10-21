59106f86aeed9ebf779b5405ed566546ae8d5da6
Pablo Quintana <pabloquintanagonzalez@MacBook-Air-de-Pablo-3.local>
2022-10-21 20:45:15 +0200

    add files

---
+++ 01Variables/main.js
@@ -0,0 +1,37 @@
+// Variable declaration
+
+let age
+
+console.log("El valor de la variable 'age' es (sin inicializar)", age)
+
+
+// Variable declaration (group)
+
+let name, year, address
+
+// Inicialization of variable 
+
+
+let teacherName = "Daniel"
+
+console.log("El nombre del profesor es", teacherName)
+
+teacherName = "Javier"
+
+console.log(`El profesor ahora se llama ${teacherName}`)
+
+
+
+//Types of variables
+
+console.log("El tipo de la variable teacherName es", typeof teacherName)
+
+teacherName = 47
+
+console.log("El tipo de variable ahora es:", typeof teacherName)
+
+teacherName = 2+2===4
+
+console.log("El tipo de variable ahora es:", typeof teacherName, "y tiene un valor de:", teacherName)
+
+
